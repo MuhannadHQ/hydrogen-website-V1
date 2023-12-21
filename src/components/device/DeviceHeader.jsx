6ae82b17127bbe622bc65eae8983b348397d1a73
MuhannadHQ <noo-oooda@hotmail.com>
2023-12-21 20:41:35 +0300

    Update DeviceHeader.jsx

--- src/components/device/DeviceHeader.jsx
+++ src/components/device/DeviceHeader.jsx
@@ -2,20 +2,20 @@ import { BuyBtn } from "components/global/BuyBtn"
 
 export const DeviceHeader = ({  device }) =>
 {
-  // const { price, title } = device || {}
-  // return (
-  //   <div className="flex general-padding justify-between py-2 bg-black text-white items-center">
-  //     <h3>
-  //       { title }
-  //     </h3>
-  //     <div className="flex gap-5 items-center">
-  //     <div>
-  //      <span className="text-xs"> ابتداء من</span>
-  //       <br/>
-  //       { price }
-  //     </div>
-  //     <BuyBtn device={ device } type={ "btn-secondary" }/>
-  //   </div>
-  //   </div>
-  // )
+  const { price, title } = device || {}
+  return (
+    <div className="flex general-padding justify-between py-2 bg-black text-white items-center">
+      <h3>
+        { title }
+      </h3>
+      <div className="flex gap-5 items-center">
+      {/* <div>
+       <span className="text-xs"> ابتداء من</span>
+        <br/>
+        { price }
+      </div> */}
+      <BuyBtn device={ device } type={ "btn-secondary" }/>
+    </div>
+    </div>
+  )
 }

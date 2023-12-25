840fc66cfecffb618faada78742f7b4d180dae7c
Ahmed Mostafa Sharabash <ahmdmsty222@gmail.com>
2023-12-25 06:57:17 +0200

    fix bug on datalayer

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import { Footer } from "components/global/Footer"
 import { Navbar } from "components/global/Navbar"
 import { Inter } from "next/font/google"
 import "styles/global.css"
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter( { subsets: [ "latin" ] } )
 
@@ -16,37 +17,37 @@ export default function RootLayout ( { children } )
   return (
     <html lang="ar">
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-NS6MLSG"></script>
-        <script dangerouslySetInnerHTML={ {
-          __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NS6MLSG');
+{/*        <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-NS6MLSG"></script>*/}
+{/*        <script dangerouslySetInnerHTML={ {*/}
+{/*          __html: `*/}
+{/*          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':*/}
+{/*new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],*/}
+{/*j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=*/}
+{/*'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);*/}
+{/*})(window,document,'script','dataLayer','GTM-NS6MLSG');*/}
 
-window.dataLayer = window.dataLayer || [];
-  function gtag(){window.dataLayer.push(arguments);}
-  gtag('js', new Date());
+{/*window.dataLayer = window.dataLayer || [];*/}
+{/*  function gtag(){window.dataLayer.push(arguments);}*/}
+{/*  gtag('js', new Date());*/}
 
-  gtag('config', 'GTM-NS6MLSG');
+{/*  gtag('config', 'GTM-NS6MLSG');*/}
 
-`} } />
-        <script dangerouslySetInnerHTML={ {
-          __html: `!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '2054771714869797');
-fbq('track', 'PageView');`
-        } } />
-        <noscript><img style={ { display: 'none' } }
-          src="https://www.facebook.com/tr?id=2054771714869797&ev=PageView&noscript=1"
-        /></noscript>
+{/*`} } />*/}
+{/*        <script dangerouslySetInnerHTML={ {*/}
+{/*          __html: `!function(f,b,e,v,n,t,s)*/}
+{/*{if(f.fbq)return;n=f.fbq=function(){n.callMethod?*/}
+{/*n.callMethod.apply(n,arguments):n.queue.push(arguments)};*/}
+{/*if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';*/}
+{/*n.queue=[];t=b.createElement(e);t.async=!0;*/}
+{/*t.src=v;s=b.getElementsByTagName(e)[0];*/}
+{/*s.parentNode.insertBefore(t,s)}(window, document,'script',*/}
+{/*'https://connect.facebook.net/en_US/fbevents.js');*/}
+{/*fbq('init', '2054771714869797');*/}
+{/*fbq('track', 'PageView');`*/}
+{/*        } } />*/}
+{/*        <noscript><img style={ { display: 'none' } }*/}
+{/*          src="https://www.facebook.com/tr?id=2054771714869797&ev=PageView&noscript=1"*/}
+{/*        /></noscript>*/}
         <title>{ metadata.title }</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -55,8 +56,8 @@ fbq('track', 'PageView');`
       </head>
 
       <body className={ inter.className }>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NS6MLSG"
-          style={ { display: 'none', visibility: 'hidden' } }></iframe></noscript>
+        {/*<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NS6MLSG"*/}
+        {/*  style={ { display: 'none', visibility: 'hidden' } }></iframe></noscript>*/}
         <Navbar />
         <main style={ {
           minHeight: "calc(100vh - 306px)",
@@ -72,7 +73,7 @@ fbq('track', 'PageView');`
             <WhattsappFloat />
           </a>
         </div>
-
+        <GoogleTagManager gtmId="GTM-XYZ" />
         <Footer />
       </body>
 

ede66d7d75f3241d42d1f557bed9e960b3136e94
MuhannadHQ <noo-oooda@hotmail.com>
2024-07-12 08:04:50 -0700

    ALL

--- src/components/Booking/BuyCard.jsx
+++ src/components/Booking/BuyCard.jsx
@@ -23,7 +23,7 @@ console.log("cart", cart)
             <div>
               <h4 className="text-primary  text-xl mb-1">{item.data.title}</h4>
 
-              <p>سعر الجهاز {item.data.devicePrice} ريال شامل الضريبة (غير شامل سعر الاشتراك)
+              <p>سعر الجهاز {item.data.devicePrice} ريال شامل الضريبة
               </p>
             </div>
             <ul className="list-disc list-inside text-start whitespace-nowrap font-normal">

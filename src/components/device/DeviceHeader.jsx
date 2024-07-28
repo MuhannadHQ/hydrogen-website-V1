b91066115181e9c33b665cc146716f5b764a3e95
Ayob <ayoubberradi>
2024-07-28 20:51:26 +0100

    perf: change title to device model in device header

--- src/components/device/DeviceHeader.jsx
+++ src/components/device/DeviceHeader.jsx
@@ -1,10 +1,11 @@
 import { BuyBtn } from "components/global/BuyBtn";
 
 export const DeviceHeader = ({ device }) => {
-  const { price, title } = device || {};
+  const { price, deviceModel } = device || {};
   return (
     <div className="flex general-padding justify-between py-2 bg-black text-white items-center">
-      <h3>{title}</h3>
+      <h3>{deviceModel}</h3>
+      <br />
       <div className="flex gap-5 items-center">
         <div>
           {/* <span className="text-xs"> ابتداء من</span> */}

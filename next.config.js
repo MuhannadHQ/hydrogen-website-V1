cd2288cfc58089028fa2eb9645c4bd1360a74d6e
MuhannadHQ <noo-oooda@hotmail.com>
2024-10-21 14:57:54 +0300

    ALL

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     BASE_URL: "https://hydrojeenapp.herokuapp.com/api/",
-    MOYASER_API_KEY: "pk_test_7CPyM7HCNY6HhUtRdnb6LMUSDDzAsyN7hXpCYMb9",
+    MOYASER_API_KEY: "pk_live_txEtTr8b5kQ1MLBdW2Wm1GF7bmcjYCoFnZGRqJQU",
     AMPLITUDE_API_KEY: "6297a7029763a9c20ede265d67862e6d",
   },
 };

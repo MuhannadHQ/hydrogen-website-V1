9e1a8965652b5142066ec711783dd486ed33d1ae
hichamDe <hicham.iamiri200@gmail.com>
2024-11-03 13:35:16 +0100

    updated the amount

--- src/app/registration-order/page.jsx
+++ src/app/registration-order/page.jsx
@@ -9,16 +9,18 @@ export default function Page() {
     const searchParams = useSearchParams();
     const orderId = searchParams.get("order_id");
     const orderDetails = searchParams.get("orderDetails");
+    const amount = 299;
     let formData = LzString.decompressFromEncodedURIComponent(orderDetails);
     formData = JSON.parse(formData);
-    formData.paymentMethod = "creditcard"
+    formData.paymentMethod = "creditcard";
+    formData.orderTotal = amount;
 
     const callbackUrl = setCallbackUrl(formData, orderId);
 
 
     return (
         <>
-            <Form callbackUrl={callbackUrl} amount={299} />
+            <Form callbackUrl={callbackUrl} amount={amount} />
         </>
     )
 }

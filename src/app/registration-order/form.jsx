f8c364bbefa50168c9dab913e1736eb656db8d28
hichamDe <hicham.iamiri200@gmail.com>
2024-11-03 11:10:04 +0100

    not fully completed

--- src/app/registration-order/form.jsx
+++ src/app/registration-order/form.jsx
@@ -1,10 +1,15 @@
 import MoyasarForm from "components/Booking/MoyasserForm";
+import { useSearchParams } from "next/navigation";
 
 export default function () {
+    const searchParams = useSearchParams();
+    const order_id = searchParams.get("order_id");
+    const package_id = searchParams.get("package_id");
+
     return (
         <MoyasarForm
-            callbackUrl="http://localhost:3000/order?orderDetails=N4IgDghgngtgpgOwC4Fk5IBYHsAmIBcIIANCAJYIDOSEANrREmVggMJYyQJQFGmUBjAMqsyvEuAgCA1hADmcAJIARXgCc4cgK4M1ErGpxw1AFSw1aBAIwAGAJx3SYNbi0CklAgG1QkGfLheFjgATTgIPVIaBGkTKDBAwgRgiQBHJB58Kyc1MgFE2wdSIwA3PLgABVz8ghtSMjxCDW1dCWdXdxRcOEtCOjAMCBAAXwBdUi0kGAIEHVpSAyM1FXFhoA"
-            amount={100}
+            callbackUrl="http://localhost:3000/other/page/for/success/page"
+            amount={299}
         />
     )
 }

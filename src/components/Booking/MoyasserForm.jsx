b5bf1cb7691891406db68ac472f6c00ef0c00129
MuhannadHQ <noo-oooda@hotmail.com>
2024-11-03 23:00:49 +0300

    ALL

--- src/components/Booking/MoyasserForm.jsx
+++ src/components/Booking/MoyasserForm.jsx
@@ -10,7 +10,7 @@ function MoyasarForm({ callbackUrl, amount }) {
       description: "Device Order #1",
 
       // publishable_api_key: process.env.MOYASER_API_KEY,
-      publishable_api_key: "pk_test_7CPyM7HCNY6HhUtRdnb6LMUSDDzAsyN7hXpCYMb9",
+      publishable_api_key: "pk_live_txEtTr8b5kQ1MLBdW2Wm1GF7bmcjYCoFnZGRqJQU",
       callback_url: callbackUrl,
       methods: ["applepay", "creditcard"],
       supported_networks: ['mada'],
